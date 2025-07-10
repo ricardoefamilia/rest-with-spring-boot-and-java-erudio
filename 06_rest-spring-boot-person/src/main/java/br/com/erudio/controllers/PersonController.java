680bb6d0c5252c4a908d10220779780825fc5f74
ricardoefamilia <ricardoefamilia@gmail.com>
2025-07-10 15:27:42 -0300

    upprojeto06

--- 06_rest-spring-boot-person/src/main/java/br/com/erudio/controllers/PersonController.java
+++ 06_rest-spring-boot-person/src/main/java/br/com/erudio/controllers/PersonController.java
@@ -1,5 +1,28 @@
 package br.com.erudio.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.erudio.model.Person;
+import br.com.erudio.services.PersonServices;
+
+@RestController
+@RequestMapping("/person")
 public class PersonController {
 
+	@Autowired
+	private PersonServices service; //injetando a partir do @Service no PersonService
+	//private PersonServices service new PersonService(); sem usar @Service no PersonService
+	
+	@RequestMapping(value = "/{id}",
+		method = RequestMethod.GET,
+		produces= MediaType.APPLICATION_JSON_VALUE
+	)
+	public Person findById(@PathVariable("id") String id) {
+		return service.findById(id);
+	}
 }
--- 06_rest-spring-boot-person/src/main/java/br/com/erudio/services/PersonServices.java
+++ 06_rest-spring-boot-person/src/main/java/br/com/erudio/services/PersonServices.java
@@ -1,5 +1,30 @@
 package br.com.erudio.services;
 
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.logging.Logger;
+
+import org.springframework.stereotype.Service;
+
+import br.com.erudio.model.Person;
+
+@Service
 public class PersonServices {
 
+	private final AtomicLong counter = new AtomicLong();
+	
+	private Logger logger = Logger.getLogger(PersonServices.class.getName());
+	
+	public Person findById(String id) {
+		logger.info("Finding one Person!");
+		
+		Person person = new Person();
+		person.setId(counter.incrementAndGet());
+		person.setFirstName("Ricardo");
+		person.setLastName("Amaral");
+		person.setAddress("Brasília - Distrito Federal - Brasil");
+		person.setGender("Male");
+		
+		return person;
+	}
+	
 }
