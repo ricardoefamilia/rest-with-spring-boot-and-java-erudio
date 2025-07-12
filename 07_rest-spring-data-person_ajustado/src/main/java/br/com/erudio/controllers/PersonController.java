ba608680b761192c9159f7381896ee09799aee5b
ricardoefamilia <ricardoefamilia@gmail.com>
2025-07-12 13:06:03 -0300

    up07_ajustado ok

--- 07_rest-spring-data-person_ajustado/src/main/java/br/com/erudio/controllers/PersonController.java
+++ 07_rest-spring-data-person_ajustado/src/main/java/br/com/erudio/controllers/PersonController.java
@@ -4,7 +4,12 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -21,24 +26,18 @@ public class PersonController {
 	private PersonServices service; //injetando a partir do @Service no PersonService
 	//private PersonServices service new PersonService(); sem usar @Service no PersonService
 	
-	@RequestMapping(value = "/{id}",
-		method = RequestMethod.GET,
-		produces= MediaType.APPLICATION_JSON_VALUE
-	)
-	public Person findById(@PathVariable("id") Long id) {
-		return service.findById(id);
-	}
-	
-	@RequestMapping(
-		method = RequestMethod.GET,
-		produces= MediaType.APPLICATION_JSON_VALUE
-	)
+	@GetMapping(produces= MediaType.APPLICATION_JSON_VALUE)
 	public List<Person> findAll() {
 		return service.findAll();
 	}
 	
-	@RequestMapping(
-		method = RequestMethod.POST,
+	@GetMapping(value = "/{id}", produces= MediaType.APPLICATION_JSON_VALUE)
+	public Person findById(@PathVariable("id") Long id) {
+		return service.findById(id);
+	}
+	
+	
+	@PostMapping(
 		consumes = MediaType.APPLICATION_JSON_VALUE,
 		produces = MediaType.APPLICATION_JSON_VALUE
 	)
@@ -46,8 +45,7 @@ public class PersonController {
 		return service.create(person);
 	}
 	
-	@RequestMapping(
-		method = RequestMethod.PUT,
+	@PutMapping(
 		consumes = MediaType.APPLICATION_JSON_VALUE,
 		produces = MediaType.APPLICATION_JSON_VALUE
 	)
@@ -55,10 +53,9 @@ public class PersonController {
 		return service.update(person);
 	}
 	
-	@RequestMapping(value = "/{id}",
-		method = RequestMethod.DELETE
-	)
-	public void delete(@PathVariable Long id) {
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<?> delete(@PathVariable Long id) {
 		service.delete(id);
+		return ResponseEntity.noContent().build();
 	}
 }
